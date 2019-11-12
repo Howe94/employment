3e92d2b8d5917c9813695d4519202b44b530e392
豪宗超 <haozch@minstone.com.cn>
2019-11-12 19:51:00 +0800

    2019-11-12

--- src/views/MenuContent/userList.vue
+++ src/views/MenuContent/userList.vue
@@ -1,86 +1,72 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <!-- <el-table-column
+  <el-row class="container">
+    <tabMenus></tabMenus>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
       label="序号"
-      width="80">
-      <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.index }}</span>
-      </template>
-    </el-table-column> -->
-    <el-table-column
-     label="序号"
-      type="index">
-    </el-table-column>
-    <el-table-column
-      label="学号">
-      <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.stuNo }}</span>
-      </template>
-    </el-table-column>
+        type="index">
+      </el-table-column>
+      <el-table-column
+        label="学号">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.stuNo }}</span>
+        </template>
+      </el-table-column>
 
-    <el-table-column
-      label="姓名">
-      <template slot-scope="scope">
-        <i class="el-icon-name"></i>
-        <span style="margin-left: 10px">{{ scope.row.name }}</span>
-      </template>
-    </el-table-column>
+      <el-table-column
+        label="姓名">
+        <template slot-scope="scope">
+          <i class="el-icon-name"></i>
+          <span style="margin-left: 10px">{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
 
-    <el-table-column
-      label="性别">
-      <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.sex }}</span>
-      </template>
-    </el-table-column>
+      <el-table-column
+        label="性别">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.sex }}</span>
+        </template>
+      </el-table-column>
 
-    <el-table-column
-      label="联系方式">
-      <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.telphone }}</span>
-      </template>
-    </el-table-column>
+      <el-table-column
+        label="联系方式">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row.telphone }}</span>
+        </template>
+      </el-table-column>
 
-    <el-table-column
-      label="住址">
-      <template slot-scope="scope">
-        <i class="el-icon-address"></i>
-        <span style="margin-left: 10px">{{ scope.row.address }}</span>
-      </template>
-    </el-table-column>
+      <el-table-column
+        label="住址">
+        <template slot-scope="scope">
+          <i class="el-icon-address"></i>
+          <span style="margin-left: 10px">{{ scope.row.address }}</span>
+        </template>
+      </el-table-column>
 
-<!-- 
-    <el-table-column
-      label="姓名"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column> -->
 
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-row>
 </template>
 
 <script>
+import tabMenus from "./tabMenus/index"
   export default {
+    components:{
+      tabMenus
+    },
     data() {
       return {
         tableData: [{
